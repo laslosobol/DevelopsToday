75b380c7c5b9548b2ed1b13b50a68600f5acb70c
sobol <laslosobol@gmail.com>
2024-05-03 22:19:41 +0300

    upd: edited datatypes and duplicates logic

--- DevelopsToday.CLI/Program.cs
+++ DevelopsToday.CLI/Program.cs
@@ -14,21 +14,19 @@ int GetIntField(CsvReader csv, string fieldName)
 
     return csv.GetField<int>(fieldName);
 }
+
 var csvFilePath = new Option<string>(
     "--csvFilePath",
     description: "Path to the CSV file."
 );
-var connectionString = new Option<string>(
-    "--connectionString",
-    description: "Connection string to the SQL Server."
-);
 var rootCommand = new RootCommand("ETL project for importing CSV data into SQL Server.")
 {
-    csvFilePath,
-    connectionString
+    csvFilePath
 };
+const string connectionString =
+    "Server=localhost;Database=DevelopsToday;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
 
-rootCommand.SetHandler((csvFilePath, connectionString) =>
+rootCommand.SetHandler((csvFilePath) =>
 {
     var records = new List<EtlRecord>();
 
@@ -42,44 +40,56 @@ rootCommand.SetHandler((csvFilePath, connectionString) =>
         {
             var record = new EtlRecord()
             {
-                tpep_pickup_datetime = csv.GetField<DateTime>("tpep_pickup_datetime"),
-                tpep_dropoff_datetime = csv.GetField<DateTime>("tpep_dropoff_datetime"),
-                passenger_count = GetIntField(csv, "passenger_count"),
-                trip_distance = csv.GetField<float>("trip_distance"),
-                store_and_fwd_flag = csv.GetField<string>("store_and_fwd_flag"),
-                PULocationID = GetIntField(csv, "PULocationID"),
-                DOLocationID = GetIntField(csv, "DOLocationID"),
-                fare_amount = csv.GetField<float>("fare_amount"),
-                tip_amount = csv.GetField<float>("tip_amount")
+                tpep_pickup_datetime = csv.GetField<DateTime?>("tpep_pickup_datetime"),
+                tpep_dropoff_datetime = csv.GetField<DateTime?>("tpep_dropoff_datetime"),
+                passenger_count = csv.GetField<int?>("passenger_count"),
+                trip_distance = csv.GetField<float?>("trip_distance"),
+                store_and_fwd_flag = csv.GetField<string?>("store_and_fwd_flag"),
+                PULocationID = csv.GetField<int?>("PULocationID"),
+                DOLocationID = csv.GetField<int?>("DOLocationID"),
+                fare_amount = csv.GetField<float?>("fare_amount"),
+                tip_amount = csv.GetField<float?>("tip_amount")
             };
 
             records.Add(record);
         }
     }
 
+    var usedKeys = new HashSet<Key>();
+    var duplicateRecords = new List<EtlRecord>();
+    var resultRecords = new List<EtlRecord>();
+    
     records.ForEach(r =>
     {
         r.store_and_fwd_flag = r.store_and_fwd_flag == "N" ? "No" : "Yes";
-        r.tpep_pickup_datetime = TimeZoneInfo.ConvertTimeToUtc(r.tpep_pickup_datetime,
-            TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
-        r.tpep_dropoff_datetime = TimeZoneInfo.ConvertTimeToUtc(r.tpep_dropoff_datetime,
-            TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+        if (r.tpep_pickup_datetime.HasValue)
+        {
+            r.tpep_pickup_datetime = TimeZoneInfo.ConvertTimeToUtc((DateTime)r.tpep_pickup_datetime,
+                TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+        }
+        if (r.tpep_dropoff_datetime.HasValue)
+        {
+            r.tpep_dropoff_datetime = TimeZoneInfo.ConvertTimeToUtc((DateTime)r.tpep_dropoff_datetime,
+                TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")); 
+        }
+
+        if (usedKeys.Contains(new Key(r.tpep_pickup_datetime, r.tpep_dropoff_datetime, r.passenger_count)))
+        {
+            duplicateRecords.Add(r);
+        }
+        else
+        {
+            resultRecords.Add(r);
+            usedKeys.Add(new Key(r.tpep_pickup_datetime, r.tpep_dropoff_datetime, r.passenger_count));
+        }
     });
-
-    var duplicateRecords = records
-        .GroupBy(r => new { r.tpep_pickup_datetime, r.tpep_dropoff_datetime, r.passenger_count })
-        .Where(g => g.Count() > 1)
-        .SelectMany(g => g.Skip(1))
-        .ToList();
-
+    
     using (var writer =
            new StreamWriter("C:\\Users\\sobol\\RiderProjects\\DevelopsToday\\DevelopsToday.CLI\\Utils\\duplicates.csv"))
     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
     {
         csv.WriteRecords(duplicateRecords);
     }
-
-    records = records.Except(duplicateRecords).ToList();
 
     using (var context = new EtlDbContext(connectionString))
     {
@@ -89,6 +99,7 @@ rootCommand.SetHandler((csvFilePath, connectionString) =>
     }
 
     Console.WriteLine($"Number of rows in the table: {records.Count}");
-},csvFilePath, connectionString);
+},csvFilePath);
 rootCommand.Invoke(args);
 //dotnet run --project "C:\Users\sobol\RiderProjects\DevelopsToday\DevelopsToday.CLI\DevelopsToday.CLI.csproj" --csvFilePath "C:\Users\sobol\RiderProjects\DevelopsToday\DevelopsToday.CLI\Utils\sample-cab-data.csv" --connectionString "Server=localhost;Database=DevelopsToday;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;"
+record Key(DateTime? tpep_pickup_datetime, DateTime? tpep_dropoff_datetime, int? passenger_count);
